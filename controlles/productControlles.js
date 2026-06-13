02a9fdebde43e3aef26d65d4f6d87dc1bf678b1c
Nikita <remnovmikita@gmail.com>
2026-06-13 14:43:50 +0300

    asd

--- controlles/productControlles.js
+++ controlles/productControlles.js
@@ -1,3 +1,4 @@
+import createHttpError from "http-errors";
 import { Product } from "../src/models/products.js";
 
 export const getAllProducts = async(req, res)=>{
@@ -13,4 +14,24 @@ export const getProductById = async(req, res)=>{
     });
   }
   res.status(200).json(product);
+};
+export const createProduct = async(req, res)=>{
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
+};
+export const deleteProducr = async(req, res)=>{
+  const {productId} = req.params;
+  const product = await Product.findOneAndDelete({_id: productId});
+  if(!product){
+    throw createHttpError(404, 'Product not found');
+  }
+  res.status(200).json(product);
+};
+export const updateProducts = async(req, res)=>{
+  const {productId}= req.params;
+  const products = await Product.findOneAndUpdate({_id: productId,  returnDocument: 'after'});
+  if(!productId){
+    throw createHttpError(404, `Note with id=${productId} not found`);
+  }
+  res.status(200).json(products);
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getAllProducts, getProductById } from "../controlles/productControlles.js";
+import { createProduct, deleteProducr, getAllProducts, getProductById, updateProducts } from "../controlles/productControlles.js";
 
 const router = Router();
 
 router.get("/products", getAllProducts);
 router.get("/products/:productId", getProductById);
+router.post("/products", createProduct);
+router.delete("/products/:productId", deleteProducr);
+router.patch("/products/:productId", updateProducts);
 
 export default router;
